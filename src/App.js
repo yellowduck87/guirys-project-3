97594bf05549daa85de327f5de6be22236b163c6
Megan Evans <megan.evans.ducky@gmail.com>
2018-11-02 19:25:03 -0600

    maybe works?

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/nav/nav'
 import Jumbotron from './components/header/header';
-import Home from './compoents/Home/home'
+import Direction from './compoents/Home/home'
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
       <div>
       <Jumbotron/>
       <Nav/>
-      <Home/>
+      <Direction/>
       </div>
       
     );
